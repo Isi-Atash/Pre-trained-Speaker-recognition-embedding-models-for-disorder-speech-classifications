433ff9391dc2ffe6f2a52c24c632829cb50080ba
Isi <70714996+Isi-Atash@users.noreply.github.com>
2022-11-05 23:36:30 +0100

    Starting from 0

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
-# import numpy as np
-# import matplotlib.pyplot as plt
 import pandas as pd
-
-# read from cdv
-ds_hun = pd.read_csv('Xvector_embedding_hun.csv')
-ds_dutch = pd.read_csv('Xvector_embedding_dutch.csv')
-
-# get the data
-# X_train = ds_hun.iloc[:, :511].values
-# y_train = ds_hun.columns['grade'].values
-X_train = ds_hun.iloc[:, :511].values
-y_train = ds_hun.label.values
-X_test = ds_dutch.iloc[:, :511].values
-y_test = ds_dutch.label.values
-
-from sklearn import svm
-
-clf = svm.SVC()
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-
-from sklearn import metrics
-
-print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
 
 
+#get th data from csv
+#2 datasets, 1 for Hungary, 1 for Dutch
+ds_org_hun = pd.read_csv('Xvector_embedding_hun.csv')
+ds_org_dutch = pd.read_csv('Xvector_embedding_dutch.csv')
 
+#Normalize the data
+#1st, we need to save only OD and HC labels from the dataset
+#2nd, we need to label_encoder the labels
+
+#find OD and HC from ds and create a new dataset
+ds_hun = ds_org_hun[(ds_org_hun.label == 'OD') | (ds_org_hun.label == 'HC')]
+ds_dutch = ds_org_dutch[(ds_org_dutch.label == 'OD') | (ds_org_dutch.label == 'HC')]
+
+#drop filename and grade columns
+ds_hun = ds_hun.drop(['filename','grade'], axis=1)
+ds_dutch = ds_dutch.drop(['filename','grade'], axis=1)
+
+#label_encoder OD and HC to 0 and 1
+labelencoder = LabelEncoder()
+ds_hun['label'] = labelencoder.fit_transform(ds_hun['label'])
+ds_dutch['label'] = labelencoder.fit_transform(ds_dutch['label'])
+
+#Scaling features to 0-1 range
+scaler = MinMaxScaler()
+ds_hun = scaler.fit_transform(ds_hun)
+print(ds_hun)
 
